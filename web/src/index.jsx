8c4fa57918bf1d31f1d9baa1fdca5c28e256b4e8
Alan Shelly <alan.m.shelly@gmail.com>
2019-09-05 16:05:33 +0900

    pass requests to App

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RPSApp from "./RPSApp";
+import Requests from 'rps/src/play'
 
-ReactDOM.render(<RPSApp/>, document.querySelector('#app'))
+ReactDOM.render(<RPSApp requests={new Requests()}/>, document.querySelector('#app'))
